1d87659e0184805c087c4bcf9e1791242baf5cd2
hdiiofficial <79190751+hdiiofficial@users.noreply.github.com>
2022-05-25 02:46:43 +0700

    Update gm-siapakahaku_ans.js

--- plugins/gm-siapakahaku_ans.js
+++ plugins/gm-siapakahaku_ans.js
@@ -3,19 +3,19 @@ const threshold = 0.72
 let handler = m => m
 handler.before = async function (m) {
     let id = m.chat
-    if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/Ketik.*who/i.test(m.quoted.contentText)) return !0
+    if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/Ketik.*ao/i.test(m.quoted.contentText)) return !0
     this.siapakahaku = this.siapakahaku ? this.siapakahaku : {}
-    if (!(id in this.siapakahaku)) return m.reply('Soal itu telah berakhir')
+    if (!(id in this.siapakahaku)) return this.sendButton(m.chat, 'Soal itu telah berakhir', wm, 'Lagi?', '.siapaaku', m)
     if (m.quoted.id == this.siapakahaku[id][0].id) {
         let json = JSON.parse(JSON.stringify(this.siapakahaku[id][1]))
-        if (['.who', 'Bantuan', ''].includes(m.text)) return !0
+        if (['.ao', 'Bantuan', ''].includes(m.text)) return !0
         if (m.text.toLowerCase() == json.jawaban.toLowerCase().trim()) {
             global.db.data.users[m.sender].exp += this.siapakahaku[id][2]
-            await this.sendButton(m.chat, `*Benar!* +${this.siapakahaku[id][2]} XP`, watermark, 'Siapakah Aku', '.siapaaku')
+            await this.sendButton(m.chat, benar + ` +${this.siapakahaku[id][2]} XP`, wm, 'Lagi?', '.siapaaku', m)
             clearTimeout(this.siapakahaku[id][3])
-            delete this.siapakahaku[id]
-        } else if (similarity(m.text.toLowerCase(), json.jawaban.toLowerCase().trim()) >= threshold) m.reply(`*Dikit Lagi!*`)
-        else m.reply(`*Salah!*`)
+            delete this.siaakahaku[id]
+        } else if (similarity(m.text.toLowerCase(), json.jawaban.toLowerCase().trim()) >= threshold) this.sendButton(m.chat, dikit, wm, 'Bantuan', '.ao', m)
+        else this.sendButton(m.chat, salah, wm, 'Bantuan', '.ao', m)
     }
     return !0
 }
