8ac5ae08683d6412ec92e52ea70854cdb7e9e0ac
ayangnyahadii <104288729+ayangnyahadii@users.noreply.github.com>
2022-05-27 02:24:37 +0700

    Create maksiat.js

---
+++ plugins/maksiat.js
@@ -0,0 +1,18 @@
+let fetch = require('node-fetch') 
+let handler  = async (m) => {
+  let res = await fetch(global.API('https://raw.githubusercontent.com', '/ayangnyahadii/database/master/scraper/premium.json'))
+  if (!res.ok) throw await res.text()
+  let json = await res.json()
+  m.reply(pickRandom(json))
+}
+handler.help = ['maksiat']
+handler.tags = ['premium']
+handler.command = /^(maksiat)$/i
+handler.register = true
+handerl.premium = true
+
+module.exports = handler
+
+function pickRandom(list) {
+  return list[Math.round(Math.random() * list.length)]
+}
