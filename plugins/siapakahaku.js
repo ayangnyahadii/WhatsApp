9b3f62d6eda2fb77250370a31211691d5b0d40ec
ayangnyahadii <104288729+ayangnyahadii@users.noreply.github.com>
2022-05-28 01:34:01 +0700

    Update siapakahaku.js

--- plugins/siapakahaku.js
+++ plugins/siapakahaku.js
@@ -5,7 +5,7 @@ let handler = async (m, { conn, usedPrefix }) => {
     conn.siapakahaku = conn.siapakahaku ? conn.siapakahaku : {}
     let id = m.chat
     if (id in conn.siapakahaku) {
-        await conn.sendButton(m.chat, 'Masih ada soal belum terjawab di chat ini', wm, 'Bantuan', usedPrefix + 'ao', conn.siapakahaku[id][0])
+        await conn.sendButton(m.chat, 'Masih ada soal belum terjawab di chat ini', wm, 'Bantuan', usedPrefix + 'saha', conn.siapakahaku[id][0])
         throw false
     }
     let src = await (await fetch('https://raw.githubusercontent.com/ayangnyahadii/database/master/games/siapakahaku.json')).json()
@@ -13,11 +13,11 @@ let handler = async (m, { conn, usedPrefix }) => {
     let caption = `
 ${json.soal}
 Timeout *${(timeout / 1000).toFixed(2)} detik*
-Ketik ${usedPrefix}ao untuk bantuan
+Ketik .saha untuk bantuan
 Bonus: ${poin} XP
     `.trim()
     conn.siapakahaku[id] = [
-        await conn.sendButtonLoc(m.chat, fla + 'Aku Siapa?', caption, wm, 'Bantuan', usedPrefix + 'ao', m),
+        await conn.sendButtonLoc(m.chat, fla + 'Aku Siapa?', caption, wm, 'Bantuan', usedPrefix + 'saha', m),
         json, poin,
         setTimeout(async () => {
             if (conn.siapakahaku[id]) await conn.sendButton(m.chat, `Waktu habis!\nJawabannya adalah *${json.jawaban}* \n *${json.deskripsi}*`, wm, 'Aku Siapa?', '.siapaaku', conn.siaakahaku[id][0])
@@ -28,5 +28,6 @@ Bonus: ${poin} XP
 handler.help = ['siapaaku']
 handler.tags = ['game']
 handler.command = /^siapa(kah)?aku/i
+handler.register = true
 
 module.exports = handler
