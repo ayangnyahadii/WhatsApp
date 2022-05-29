ba8ca3e47808707f1b157c0861858d4de8038f14
ayangnyahadii <104288729+ayangnyahadii@users.noreply.github.com>
2022-05-30 02:07:48 +0700

    Update sourcecode.js

--- plugins/sourcecode.js
+++ plugins/sourcecode.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn }) => {
-     conn.reply(m.chat, `Bot ini Menggunakan Script Private, Jika ingin Script Seperti ini bisa cek Script ORI cari sja di github Family-Md ,makasi!`, m)
+     conn.reply(m.chat, `BOT INI MENGGUNAKAN REPOSITORY/SOURCE CODE PRIVATE!!!\n\nJIKA ANDA INGIN MEMBUAT BOT SEPERTI BOT INI ANDA BISA MENGGUNAKAN SOURCE CODE / REPOSITORY ORIGINAL DARI BOT INI.\n\nTHANKS TO\nFAMILY-MD\n• https://github.com/fokusdotid/Family-Md\n\nJangan lupa kalo pake fork dan starnya lu itu cuma make seengaknya ngehargain yang bikin!!`, global.ftroli)
 }
 handler.help = ['sourcecode']
 handler.tags = ['info']
